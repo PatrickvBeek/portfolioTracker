304d1cc8d0584dd62e1c15b558afaaf4e1292115
Patrick van Beek <pvanbeek@vivaldi.net>
2023-08-11 17:20:48 +0200

    fix tests

--- client/src/testUtils.ts
+++ client/src/testUtils.ts
@@ -1,6 +1,7 @@
 import { useGetAssets } from "./hooks/assets/assetHooks";
 import {
   useAddOrderToPortfolio,
+  useGetPortfolio,
   useGetPortfolios,
 } from "./hooks/portfolios/portfolioHooks";
 
@@ -8,7 +9,13 @@ const mockUseGetAssets = useGetAssets as jest.Mock<any>;
 jest.mock("./hooks/assets/assetHooks");
 
 const mockUseGetPortfolios = useGetPortfolios as jest.Mock<any>;
+const mockUseGetPortfolio = useGetPortfolio as jest.Mock<any>;
 const mockUseAddOrder = useAddOrderToPortfolio as jest.Mock<any>;
 jest.mock("./hooks/portfolios/portfolioHooks");
 
-export { mockUseGetAssets, mockUseGetPortfolios, mockUseAddOrder };
+export {
+  mockUseAddOrder,
+  mockUseGetAssets,
+  mockUseGetPortfolio,
+  mockUseGetPortfolios,
+};
