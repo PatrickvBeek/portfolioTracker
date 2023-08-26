eb6257d3ab41fdb154a38399e3df8c728dcde39a
Patrick van Beek <pvanbeek@vivaldi.net>
2023-08-26 18:32:22 +0200

    rename client folder to frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import express from "express";
 import { config } from "./config";
 import { app } from "./index";
 
-app.use(express.static("../client/build"));
+app.use(express.static("../frontend/build"));
 
 app.listen(config.PORT, () => console.log(`Listening on port ${config.PORT}`));
