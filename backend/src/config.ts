4ea6069bb60a04175bd3a9f4ffbee6a79953f9bf
Patrick van Beek <pvanbeek@vivaldi.net>
2025-01-04 12:49:39 +0100

    remove endpoints from backend

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,9 +1,5 @@
-import path from "path";
-
 export const config = {
   NODE_ENV: process.env.NODE_ENV || "development",
   HOST: process.env.HOST || "127.0.0.1",
   PORT: process.env.PORT || "6060",
-  DATA_DIR:
-    process.env.PT_DATA_DIR || path.join(__filename, "../../../../../data"),
 };
