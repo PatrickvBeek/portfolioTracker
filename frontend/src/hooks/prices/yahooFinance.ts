42e17e639ff5134921cbe555824a1faeef56af99
Patrick van Beek <pvanbeek@vivaldi.net>
2025-02-22 19:27:33 +0100

    use prices from yahoo

--- frontend/src/hooks/prices/yahooFinance.ts
+++ frontend/src/hooks/prices/yahooFinance.ts
@@ -1,21 +1,29 @@
 import { History } from "pt-domain/src/portfolioHistory/history.entities";
 import { zip } from "radash";
 
-const BASE_URL = "https://query2.finance.yahoo.com";
+const BASE_URL = "https://yfapi.net";
 
-export const getPricesFromYahooFinance = async (
-  symbol: string
-): Promise<History<number>> => {
-  const response = await fetch(
-    `${BASE_URL}/v8/finance/chart/${symbol}?period1=0&period2=9999999999&interval=1d&includePrePost=true&events=div%7Csplit`
-  );
-  const parsed = (await response.json()) as YahooChartResponse;
+export const getPricesFromYahooFinance =
+  (apiKey: string) =>
+  async (symbol: string): Promise<History<number>> => {
+    const response = await fetch(
+      `${BASE_URL}/v8/finance/chart/${symbol}?range=10y&interval=1d&includePrePost=true&events=div%7Csplit`,
+      {
+        headers: {
+          accept: "application/json",
+          "X-API-KEY": apiKey,
+        },
+      }
+    );
+    const parsed = (await response.json()) as YahooChartResponse;
 
-  return zip(
-    parsed.chart.result[0]?.timestamp,
-    parsed.chart.result[0]?.indicators.adjclose[0]?.adjclose
-  ).map(([timestamp, value]) => ({ timestamp, value }));
-};
+    return zip(
+      parsed.chart.result[0]?.timestamp,
+      parsed.chart.result[0]?.indicators.adjclose[0]?.adjclose
+    )
+      .map(([t_in_s, price]) => ({ timestamp: t_in_s * 1000, value: price }))
+      .toReversed();
+  };
 
 type YahooChartResponse = {
   chart: {
