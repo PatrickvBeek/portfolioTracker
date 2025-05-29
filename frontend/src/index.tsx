9746bbf17993fca0513c1f5869125d42d6a49fc5
Patrick van Beek <pvanbeek@vivaldi.net>
2025-05-29 10:40:27 +0200

    upgrade all dependencies and remove web-vitals

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,10 +2,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
 root.render(<App />);
-
-reportWebVitals();
